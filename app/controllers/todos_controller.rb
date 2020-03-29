40216f983b78a262717d7344eac454ecb5461db5
svr011 <svivekanandareddy5@gmail.com>
2020-03-29 23:22:35 +0530

    Add ability to create a new todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,15 @@ class TodosController < ApplicationController
         todo=Todo.find(id)
         render plain: todo.to_pleasant_string
     end
+    def create
+        todo_text=params[:todo_text]
+        due_date=DateTime.parse(params[:due_date])
+        new_todo=Todo.create!(
+            todo_text:todo_text,
+            due_date:due_date,
+            completed:false,
+        )
+        response="hey id of new todo is #{new_todo.id}"
+        render plain:response
+    end
 end

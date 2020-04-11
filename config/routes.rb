44ecba5be6e53b6eaf09883f326e972ce2726bb2
svr011 <svivekanandareddy5@gmail.com>
2020-04-11 18:23:04 +0530

    HTML and CSS files added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "users",to:"users#index"
-  post "users/create",to:"users#create"
-  get "users/:id",to:"users#show"
+  #get "users",to:"users#index"
+  #post "users/create",to:"users#create"
+  #get "users/:id",to:"users#show"
   resources :todos
-  #resources:users
+  resources :users
   post "users/login", to: "users#login"
   
 end

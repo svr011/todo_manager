6a48749af71122639103504d2eec9d59eb5c6c29
svr011 <svivekanandareddy5@gmail.com>
2020-04-17 22:45:27 +0530

    Use the form_with helper and ensure CSRF attacks are prevented

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,4 @@
 class TodosController < ApplicationController
-    skip_before_action :verify_authenticity_token
     def index
         #render plain:Todo.order(:due_date).map{ |todo| todo.to_pleasant_string}.join("\n")
         render "index"
@@ -27,6 +26,11 @@ class TodosController < ApplicationController
         todo.save!
         redirect_to todos_path
     end
-    
+    def destroy
+        id = params[:id]
+        todo = Todo.find(id)
+        todo.destroy
+        redirect_to todos_path
+      end
 
 end

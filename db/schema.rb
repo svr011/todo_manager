10631eb5376c5ac61dc64eb37f2757c4080e5037
svr011 <svivekanandareddy5@gmail.com>
2020-04-18 00:16:56 +0530

    Added migration to create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_180325) do
+ActiveRecord::Schema.define(version: 2020_04_17_183654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_04_14_180325) do
     t.text "todo_text"
     t.date "due_date"
     t.boolean "completed"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
